7fa97a5b48167ece9a0666ad1a26ac1fa69976b9
GREG8R <grigoriy.kolotilin@gett.com>
2019-12-06 00:45:06 +0300

    fix format

--- hwAlgebraicAlgo/main.go
+++ hwAlgebraicAlgo/main.go
@@ -88,7 +88,7 @@ func powerLaunch(){
 
 	number = 2
 	pow = 20
-	fmt.Printf("power test number = %f, power = %\n", number, pow)
+	fmt.Printf("power test number = %f, power = %d\n", number, pow)
 	t = time.Now()
 	result = PowOfPowTwo(number, pow)
 	fmt.Printf("power of power two result: %f, time: %d\n", result, time.Now().Sub(t).Nanoseconds())
